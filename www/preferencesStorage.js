a0023ab5d9be8b7323557161740ea310d6d5ac65
satish kumar yadav <satishSKY@users.noreply.github.com>
2015-04-09 15:08:20 +0530

    Update preferencesStorage.js

--- www/preferencesStorage.js
+++ www/preferencesStorage.js
@@ -4,9 +4,7 @@
 	            callback('Nothing to echo.');
 	        }, "PreferencesStorage", "getAll", []);
 	    };
-	    window.getAll(function(echoValue) {
-	       console.log(echoValue); // should alert true.
-	    });
+	  
 	
 
 	    
@@ -16,9 +14,7 @@
 	            callback('Nothing to echo.');
 	        }, "PreferencesStorage", "storeMsg", keyValue);
 	    };
-	    /*   window.storeMsg( ["userName","sky"],function(echoValue) {
-        alert(echoValue); // should alert true.
-    });*/
+	 
 	    
 	    
 	    window.getMsgByKey = function(key ,callback) {
@@ -26,27 +22,18 @@
 	            callback('Nothing to echo.');
 	        }, "PreferencesStorage", "getMsg", [key]);
 	    };
-	   /* window.getMsgByKey( "userName",function(echoValue) {
-	        alert(echoValue); // should alert true.
-	    });*/
-	    
+	  
 	    
 	    window.removeMsgByKey = function(key,callback) {
 	        cordova.exec(callback, function(err) {
 	            callback('Nothing to echo.');
 	        }, "PreferencesStorage", "removeMsg", [key]);
 	    };
-	/*    window.removeMsgByKey("userName", function(echoValue) {
-	        alert(echoValue); // should alert true.
-	    });
-	*/
+
 	    window.removeAll = function(callback) {
 	        cordova.exec(callback, function(err) {
 	            callback('Nothing to echo.');
 	        }, "PreferencesStorage", "removeAll", [key]);
 	    };
-	   /* window.removeAll( function(echoValue) {
-	        alert(echoValue); // should alert true.
-	    });*/
-	   	
+	  
 
